d699a9877393b8889a731d03d6bad02f7012466d
harsh021102 <harsh021102@gmail.com>
2023-01-18 22:21:44 +0530

    d

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import flower from './image/flower.jpg'
-import explain from './image/explain.gif'
 function App() {
   const [index,setIndex] = useState(1);
   const [movement,setMovement] = useState('');
@@ -19,13 +18,13 @@ function App() {
   return (
     <div className="h-screen flex justify-center items-center bg-gray-300">
       <div className="h-full py-8 sm:h-full w-full sm:w-1/2 flex flex-col justify-center items-center bg-green-300">
-        <img src={explain} className="rounded-lg border-2 border-black justify-center items-center w-8/12" alt="flower"/>
+        <img src={flower} className="rounded-lg border-2 border-black justify-center items-center w-8/12" alt="flower"/>
         <div className='w-full h-full flex'>
           <div className='h-full w-1/2 flex justify-center items-center'>
-            <button className='bg-blue-300 p-2 sm:p-4 rounded-lg border-2 border-white text-lg text-white font-bold' onClick={()=>{alert('Tum bahut achi ho Deepakshi')}}>Sure, I will love it!😊</button>
+            <button className='bg-blue-300 p-2 sm:p-4 rounded-lg border-2 border-white text-lg text-white font-bold' onClick={()=>{alert('Thank you for accepting it😊😂. Bye Bye!')}}>Yes I accept your flower!😊</button>
           </div>
           <div className='h-full w-1/2 flex justify-center items-center relative'>
-            <button className={`p-2 sm:p-4 rounded-lg border-2 border-white text-lg text-white font-bold bg-blue-300 absolute ${movement}`} onClick={move} onMouseEnter={move}>Naaaaaaa😒</button>
+            <button className={`p-2 sm:p-4 rounded-lg border-2 border-white text-lg text-white font-bold bg-blue-300 absolute ${movement}`} onClick={move} onMouseEnter={move}>No😒</button>
           </div>
         </div>
       </div>
